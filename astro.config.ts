da70231fcb2bdda61ef8d56bffbd9c49c2ac275a
crllect <crllect@proton.me>
2026-09-13 22:56:48 -0400

    added some boilerplate

---
+++ astro.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import preact from "@astrojs/preact";
+
+export default defineConfig({
+	output: "static",
+	adapter: node({ mode: "standalone" }),
+	integrations: [preact()],
+	trailingSlash: "never",
+	build: {
+		format: "file",
+		inlineStylesheets: "auto"
+	},
+	prefetch: {
+		prefetchAll: true,
+		defaultStrategy: "hover"
+	},
+	server: {
+		host: true
+	},
+	devToolbar: {
+		enabled: false
+	},
+	security: {
+		checkOrigin: true
+	},
+	vite: {
+		ssr: {
+			external: ["puppeteer", "puppeteer-extra", "puppeteer-extra-plugin-stealth"]
+		}
+	}
+});
